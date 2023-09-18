3856397562f8758f487fd23e858962f08523ab2c
philsv <35413649+philsv@users.noreply.github.com>
2023-09-18 19:33:30 +0200

    refactor: init commit

---
+++ app/components/pages.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <main>
+        <h1>Hello Next 13 🔥</h1>
+      </main>
+    )
+  }
+  
--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   swcMinify: true,
+  images: {
+    domains: ['lh3.googleusercontent.com']
+  },
 };
 
 module.exports = nextConfig;
